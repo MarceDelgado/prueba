6f7d8c8b680d331d222f444e6a7c58e0ccee1ca5
MarceDelgado <madelgado1@udc.edu.ar>
2025-10-22 11:41:42 -0300

    url crear peronas

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import (home, buscar_animales,contacto, login_view, logout_view, registro, dashboard, eliminar_raza, crear_raza, eliminar_persona,
                     ListarMascotas, CrearMascota, ModificarMascota, EliminarMascota, ModificarEspecieView)
-from apps.core.views import ListarEspeciesView
+from apps.core.views import ListarEspeciesView, crear_persona
 
 urlpatterns = [
 	path('', home, name='home'),
@@ -19,6 +19,7 @@ urlpatterns = [
     path('eliminar/<int:pk>/', EliminarMascota.as_view(), name='eliminar_mascotas'),
     path('eliminar_raza/<int:raza_id>/', auth_views.eliminar_raza, name='eliminar_raza'),
     path('razas/', crear_raza, name = 'crear_raza'),
+	 path('personas/crear/', crear_persona, name='crear_persona'),
     path('persona/eliminar/<int:persona_id>/', eliminar_persona, name='eliminar_persona'),
 	path('especies/', ListarEspeciesView.as_view(), name='listar_especies'),
     path('especie/modificar/<int:pk>/', ModificarEspecieView.as_view(), name='modificar_especie'),
