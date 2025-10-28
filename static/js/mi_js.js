f37250bab5a634f6b784e40bef496dac8f73d183
MarceDelgado <madelgado1@udc.edu.ar>
2025-10-28 15:53:21 -0300

    acordeon

--- static/js/mi_js.js
+++ static/js/mi_js.js
@@ -1,25 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const accordionButtons = document.querySelectorAll(".accordion-button");
+document.addEventListener('DOMContentLoaded', function() {
+    const accordionButtons = document.querySelectorAll('.accordion-button');
 
     accordionButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            // Toggle activo para el botón
-            this.classList.toggle("active");
-
-            // Obtén el contenido del acordeón correspondiente
-            const content = this.nextElementSibling;
-
-            // Alterna la clase "open" para el contenido
-            content.classList.toggle("open");
-
-            // Cierra todos los otros acordeones
-            accordionButtons.forEach(otherButton => {
-                if (otherButton !== this) {
-                    otherButton.classList.remove("active");
-                    otherButton.nextElementSibling.classList.remove("open");
-                }
-            });
+        button.addEventListener('click', function() {
+            const icon = button.querySelector('.toggle-icon');
+            icon.classList.toggle('rotate');
         });
     });
 });
-
